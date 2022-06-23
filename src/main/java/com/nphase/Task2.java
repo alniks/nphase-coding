e5a91a188885a3e001006b8b987db62b7ad569e8
Dzmitry_Kashlach <Bear doesn't like honey>
2022-06-23 13:51:38 +0300

    task 2

--- src/main/java/com/nphase/Task2.java
+++ src/main/java/com/nphase/Task2.java
@@ -8,18 +8,22 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
         List<Product> productList = new ArrayList<>();
+
+        Product oneTea = new Product("tea", new BigDecimal(5), 1);
+        Product twoCoffee = new Product("coffee", new BigDecimal(3.5), 2);
         ProductBuilder builder = new ProductBuilder();
-        Product oneTea = builder.build("tea", new BigDecimal(5), 1);
-        Product twoCoffee = builder.build("coffee", new BigDecimal(3.5), 2);
-        productList.add(oneTea);
-        productList.add(twoCoffee);
+        Product fiveTea = builder.build("tea", new BigDecimal(5), 5);
+        Product threeCoffee = builder.build("tea", new BigDecimal(3.5), 3);
+        productList.add(fiveTea);
+        productList.add(threeCoffee);
         ShoppingCart shoppingCart = new ShoppingCart(productList);
         ShoppingCartService cartService = new ShoppingCartService();
         cartService.calculateTotalPrice(shoppingCart);
         BigDecimal total = cartService.calculateTotalPrice(shoppingCart);
+
         System.out.println("Shopping cart total = " + total);
     }
 }
