c1130657231a34f2e7fcdfa57bd404e4f59c40bf
Eugene Putsykovich <eugene.putsykovich@33across.com>
2023-02-21 14:30:41 +0300

    Completed task 2

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -3,11 +3,20 @@ package com.nphase.service;
 import com.nphase.entity.ShoppingCart;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ShoppingCartService {
+    private static final double DISCOUNT_FACTOR = 0.9D;
+
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
-        return shoppingCart.getProducts().stream()
+        BigDecimal totalPrice = shoppingCart.getProducts().stream()
                 .map(it -> it.getPricePerUnit().multiply(BigDecimal.valueOf(it.getQuantity())))
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal finaPrice = shoppingCart.getProducts().size() > 3
+                ? totalPrice.multiply(BigDecimal.valueOf(DISCOUNT_FACTOR))
+                : totalPrice;
+
+        return finaPrice.setScale(2, RoundingMode.HALF_UP);
     }
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -24,4 +24,18 @@ public class ShoppingCartServiceTest {
 
         Assertions.assertEquals(BigDecimal.valueOf(16.5).setScale(2, RoundingMode.HALF_UP), result);
     }
+
+    @Test
+    void calculatePriceWithDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(2.5D), 1),
+                new Product("Coffee", BigDecimal.valueOf(1), 1),
+                new Product("Cake", BigDecimal.valueOf(4), 1),
+                new Product("Apple", BigDecimal.valueOf(2.5D), 1)
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        Assertions.assertEquals(BigDecimal.valueOf(9).setScale(2, RoundingMode.HALF_UP), result);
+    }
 }
