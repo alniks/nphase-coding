0285da2c96b19f5ab191f418c93f721a3f71e455
andrey-yatsyna-m10 <andrey.yatsyna@pashapay.az>
2023-10-27 13:25:48 +0300

    test tasks

---
+++ src/main/java/com/nphase/config/DiscountConfig.java
@@ -0,0 +1,13 @@
+package com.nphase.config;
+
+import java.math.BigDecimal;
+
+public class DiscountConfig {
+
+    public static class Discount{
+        private String category;
+        private Long countItems;
+        private BigDecimal discount;
+
+    }
+}
--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,12 +1,26 @@
 package com.nphase.service;
 
 import com.nphase.entity.ShoppingCart;
-
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCartService {
 
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
-        throw new UnsupportedOperationException("not supported");
+        Map<String, Integer> category = new HashMap<>();
+        shoppingCart.getProducts().forEach(product -> {
+            Integer count = product.getQuantity() + category.getOrDefault(product.getCategory(), 0);
+            category.put(product.getCategory(), count);
+        });
+
+        return shoppingCart.getProducts().stream()
+                .map(product -> {
+                    BigDecimal price = category.get(product.getCategory()) <= 3
+                            ? product.getPricePerUnit()
+                            : product.getPricePerUnit().multiply(BigDecimal.valueOf(0.9d));
+                    return price.multiply(BigDecimal.valueOf(product.getQuantity()));
+                })
+                .reduce(BigDecimal.ZERO, (p, q) -> p.add(q));
     }
 }
